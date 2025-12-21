42b8aa3d47903316ba78ac17461611e7033ffe8b
Ahsan Habib <habib23105341021@diu.edu.bd>
2025-12-21 16:39:48 +0600

    enrolled done

--- index.js
+++ index.js
@@ -28,67 +28,97 @@ async function run() {
     const db = client.db('E-learning');
     const courseCollection = db.collection('courses');
     const instractorCollection = db.collection('instructor')
+    const enrolledCourseCollection = db.collection('enrolledCourse')
 
     // course related api 
-    app.get('/courses', async(req, res) => {
-        const cursor = courseCollection.find();
-        const result = await cursor.toArray()
-        res.send(result)
+    app.get('/courses', async (req, res) => {
+      const cursor = courseCollection.find();
+      const result = await cursor.toArray()
+      res.send(result)
     })
-    app.get('/course/:id', async(req, res) => {
-        const {id} = req.params;
-        const result = await courseCollection.findOne({_id: new ObjectId(id)});
-        res.send(result)
+    app.get('/course/:id', async (req, res) => {
+      const { id } = req.params;
+      const result = await courseCollection.findOne({ _id: new ObjectId(id) });
+      res.send(result)
     })
 
 
     // my Course
-    app.get('/my-courses', async(req, res) => {
+    app.get('/my-courses', async (req, res) => {
       const email = req.query.email;
       const result = await courseCollection.find({ email }).toArray();
       res.send(result);
     })
 
-    app.get('/top-courses', async(req,res) => {
-      const cursor = courseCollection.find().sort({ratings: -1}).limit(3);
+    app.get('/top-courses', async (req, res) => {
+      const cursor = courseCollection.find().sort({ ratings: -1 }).limit(3);
       const result = await cursor.toArray();
       res.send(result)
     })
 
-    app.post('/courses', async(req, res) => {
-        const courseInfo = req.body;
-        const result = await courseCollection.insertOne(courseInfo);
-        res.send(result);
-    })
-
-    // Course Details
-    app.get('/viewDetails/:id', async(req, res) => {
-      const {id} = req.params;
-      const result = await courseCollection.findOne({_id : new ObjectId(id)})
+    app.post('/courses', async (req, res) => {
+      const courseInfo = req.body;
+      const result = await courseCollection.insertOne(courseInfo);
       res.send(result);
     })
 
-    app.get('/topInstructors', async(req, res)=> {
-      const cursor = instractorCollection.find().sort({ratings: -1}).limit(4);
+    // Course Details
+    app.get('/viewDetails/:id', async (req, res) => {
+      const { id } = req.params;
+      const result = await courseCollection.findOne({ _id: new ObjectId(id) })
+      res.send(result);
+    })
+
+    app.get('/topInstructors', async (req, res) => {
+      const cursor = instractorCollection.find().sort({ ratings: -1 }).limit(4);
       const result = await cursor.toArray();
       res.send(result);
     })
 
     // Delete My Courses
-    app.delete('/courses/:id', async(req, res) => {
+    app.delete('/courses/:id', async (req, res) => {
       const id = req.params;
-      const result = await courseCollection.deleteOne({_id: new ObjectId(id)})
+      const result = await courseCollection.deleteOne({ _id: new ObjectId(id) })
       res.send(result)
     })
 
 
     // Edit My Course
-    app.patch('/edit-course/:id', async(req, res) =>{
-      const {id} = req.params;
+    app.patch('/edit-course/:id', async (req, res) => {
+      const { id } = req.params;
       const data = req.body;
-      const result = await courseCollection.updateOne({_id: new ObjectId(id)}, {$set: data})
+      const result = await courseCollection.updateOne({ _id: new ObjectId(id) }, { $set: data })
       res.send(result)
     })
+
+    // My Enroll Course
+    app.post('/my-enrolled-course', async (req, res) => {
+      const { courseId, userEmail } = req.body;
+
+      const exist = await enrolledCourseCollection.findOne({ courseId, userEmail })
+
+      if (exist) {
+        return res.send({ enrolled: true, message: 'user is already enrolled to the course' })
+      }
+
+      const result = await enrolledCourseCollection.insertOne(req.body)
+      res.send(result);
+    })
+    
+    app.get('/my-enrolled-course', async (req, res) => {
+      const email = req.query.email;
+
+      if (!email) {
+        return res.send([]);
+      }
+
+      const result = await enrolledCourseCollection
+        .find({ userEmail: email })
+        .toArray();
+
+      res.send(result);
+    });
+
 
   } finally {
     // Optional: you can close the client if you want to stop the server
@@ -99,7 +129,7 @@ async function run() {
 run().catch(console.dir);
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+  res.send('Hello World!')
 })
 
 app.listen(port, () => {
